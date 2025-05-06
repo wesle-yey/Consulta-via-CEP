95b8df30d4ee555c91b094ad82db119022561fb1
wesle-yey <clashcomifeeh@gmail.com>
2025-05-06 10:08:08 -0300

    Projeto para consulta de clientes via CEP

---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.lab_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/one/digitalinnovation/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.lab_padroes_projeto_spring.service;
+
+import one.digitalinnovation.lab_padroes_projeto_spring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
